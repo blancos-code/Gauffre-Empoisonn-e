47e5ecd72c1d1714775955543b8c03ebe385593d
Killian Cengo <cengokillian@hotmail.com>
2023-04-22 10:50:03 +0200

    Modification de l'icône de la fenêtre et ajout de quitter le jeu quand on clique sur quitter

--- src/Vue/MenuListener.java
+++ src/Vue/MenuListener.java
@@ -74,6 +74,9 @@ public class MenuListener implements MouseListener {
                 throw new RuntimeException(ex);
             }
         }
+        if(estCurseurSurBouton_Quitter(e)){
+            System.exit(0);
+        }
         m.repaint();
     }
 
