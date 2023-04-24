f8fe2c6cded37c40d28f4c614549b258b3e62367
Killian Cengo <cengokillian@hotmail.com>
2023-04-24 09:40:41 +0200

    début de l'implémentation de MinMax

---
+++ src/Modele/IAResolveur.java
@@ -0,0 +1,52 @@
+package Modele;
+
+import java.util.HashMap;
+
+public class IAResolveur extends IA{
+
+    //this.jeu.gaufre() est déjà disponible puisque on extends IA
+
+
+    public IAResolveur() {
+        super();
+    }
+
+    public Coup joue() {
+        int valeur = calcul_Joueur_A(jeu.gaufre().getCases(), 1);
+        return jouerCoup(jeu.gaufre().getCases(), valeur);
+    }
+
+    public Coup jouerCoup(int[][] configuration, int valeur){
+        return null;
+    }
+
+    public int evaluation(int[][] configuration){
+        return 0;
+    }
+
+    public Coup coupsJouables(int[][] configuration){
+
+    }
+
+    public int calcul_Joueur_A(int[][] configuration, int horizon){
+        if(horizon == 0){
+            return evaluation(configuration);
+        }else {//le joueur A doit jouer
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < configuration.length; i++) {
+                for (int j = 0; j < configuration[0].length; j++) {
+                    if(configuration[i][j] == 0){
+                        configuration[i][j] = 1;
+                        int min = calcul_Joueur_B(configuration,horizon-1);
+                        if(min > max){
+                            max = min;
+                        }
+                        configuration[i][j] = 0;
+                    }
+                }
+            }
+            return max;
+        }
+        return 0;
+    }
+}
